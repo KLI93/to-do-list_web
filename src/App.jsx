acdf791a5d40e89c94f7c54368c7667e24afbd7b
Никита Клименко <nnosfer@bk.ru>
2026-05-15 01:34:29 +0300

    Fix background video path

--- src/App.jsx
+++ src/App.jsx
@@ -133,7 +133,7 @@ function App() {
     <>
      <div className="video-background">
         <video autoPlay loop muted playsInline>
-          <source src="/background-video.mp4" type="video/mp4" />
+          <source src="background-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="overlay"></div> {/* Затемнение */}
